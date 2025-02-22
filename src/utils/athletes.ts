4eca3ad37033b19cc56774d0d337194fc1d77dd1
Aditi Joshi <ad.joshi@outlook.com>
2025-02-21 16:12:06 -0800

    added uw athletes

--- src/utils/athletes.ts
+++ src/utils/athletes.ts
@@ -1,56 +1,34 @@
 export const athletes = [
     {
-        name: "John Doe",
-        sport: "Football",
-        image: "/athlete1.jpg",
+        name: "Molly Wilson",
+        sport: "Women's Volleyball (Setter)",
+        image: "/molly.jpg",
         social: {
-            instagram: { username: "johndoe", followers: 10.5 },
-            tiktok: { username: "johndoe", followers: 20.1 }
+            instagram: { username: "mollyraewilson", followers: 2.1 }
         }
     },
     {
-        name: "Jane Smith",
-        sport: "Basketball",
-        image: "/athlete1.jpg",
+        name: "Will Landram",
+        sport: "Men's Basketball (Forward, Guard)",
+        image: "/will.jpg",
         social: {
-            instagram: { username: "janesmith", followers: 15.3 },
-            tiktok: { username: "janesmith", followers: 25.4 }
+            instagram: { username: "will.landram", followers: 1 }
         }
     },
     {
-        name: "Mike Johnson",
-        sport: "Baseball",
-        image: "/athlete1.jpg",
+        name: "Maya Loudd",
+        sport: "Women's Soccer (Center Back)",
+        image: "/maya.jpg",
         social: {
-            instagram: { username: "mikejohnson", followers: 8.7 },
-            tiktok: { username: "mikejohnson", followers: 18.2 }
+            instagram: { username: "maya.loudd", followers: 1.3 }
         }
     },
     {
-        name: "Emily Davis",
-        sport: "Soccer",
-        image: "/athlete1.jpg",
+        name: "Lana Navarro",
+        sport: "Women's Gymnastics (Beam, Floor, Vault)",
+        image: "/lana.jpg",
         social: {
-            instagram: { username: "emilydavis", followers: 12.1 },
-            tiktok: { username: "emilydavis", followers: 22.3 }
-        }
-    },
-    {
-        name: "Chris Lee",
-        sport: "Swimming",
-        image: "/athlete1.jpg",
-        social: {
-            instagram: { username: "chrislee", followers: 9.4 },
-            tiktok: { username: "chrislee", followers: 19.6 }
-        }
-    },
-    {
-        name: "Sarah Brown",
-        sport: "Tennis",
-        image: "/athlete1.jpg",
-        social: {
-            instagram: { username: "sarahbrown", followers: 11.8 },
-            tiktok: { username: "sarahbrown", followers: 21.7 }
+            instagram: { username: "lanaxnavarro", followers: 2.5 }
         }
     }
 ];
