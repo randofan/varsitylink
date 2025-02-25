f896a69f351d9012b5cb0c381254addb127f6655
David Song <davsong@outlook.com>
2025-02-25 14:51:39 -0800

    pushed types

---
+++ src/utils/types.ts
@@ -0,0 +1,44 @@
+// Only includes intermediate types.
+// For final types (i.e. anything getting added to the DB), use
+// the types from prisma/schema.prisma
+// For example, `import { type } from '@prisma/client';`
+
+// There are no intermediate types (for now) for businesses and
+// students, so we use the prisma types directly for both.
+
+import { Compensations } from "@prisma/client";
+
+
+export interface CampaignFormData {
+    name: string;
+    campaignSummary: string;
+    maxBudget: string;  // can be $$$ amount or in-kind
+    compensation: Compensations;
+    studentAthleteCount: string;  // can be a number of 'any', in which case we'll choose
+    sports: string[];
+    startDate: Date;
+    endDate: Date;
+}
+
+export interface GeneratedCampaign {
+    objectives: string;
+    targetAudience: string;
+    channels: string;
+    timeline: string;
+    budgetBreakdown: string;
+    creativeConcept: string;
+    metrics: string;
+}
+
+export const sportsOptions = [
+    'Baseball',
+    'Basketball',
+    'Football',
+    'Golf',
+    'Soccer',
+    'Tennis',
+    'Track and Field',
+    'Volleyball',
+    'Wrestling',
+    'Other'
+];
